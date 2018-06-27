0c959294abe5b17f478ccbc99ac841df3710bb18
shaobaobaoer <shaobaobaoer@126.com>
2018-06-27 21:28:10 +0800

    activity add testing data

--- src/components/enroll_in_card.vue
+++ src/components/enroll_in_card.vue
@@ -109,7 +109,28 @@
         activity_name_list: [],
         index: 0,
         show: true,
-        activities: [],
+        activities: [
+          {
+            id: 1,
+            title: '计算机学院开源社区夏季学期活动',
+            founder: {
+              avatar: '',
+              name: ''
+            },
+            poster: `http://www.sz.shu.edu.cn/Resources/ZuZTX/sdlogo.jpg`,
+            views: 100,
+            enrolls: 50,
+            // favoriteTime: rawActivity.ShouCRS,
+            capacity: 100,
+            location: '宝山东校区',
+            favorite: true,
+            register: false,
+            start: '2018-7-1',
+            end: '2018-7-31',
+            detail: '<p style="text-underline-color: #0bb20c">这是一条测试数据</p>',
+            notice: ''
+          }
+        ],
         activity: {},
         favoriteActivities: [],
         enrollsActivities: [],
